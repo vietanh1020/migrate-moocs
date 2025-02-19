99dbc62a4a987858aa5203b53c954bf7bf0a4671
AnhVV <anh.voviet@mobifone.vn>
2025-02-19 16:40:50 +0700

    done news

---
+++ news.js
@@ -0,0 +1,94 @@
+import mysql from "mysql2/promise";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+// Load biến môi trường từ .env
+dotenv.config();
+
+// Đọc cấu hình từ .env
+const {
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE_NEWS,
+    MONGO_URL,
+    MONGO_DATABASE,
+    BATCH_SIZE,
+    NEW_SITE_ID,
+    OLD_SITE_ID,
+} = process.env;
+
+
+// Kết nối MySQL
+async function connectMySQL() {
+    return mysql.createConnection({
+        host: MYSQL_HOST,
+        user: MYSQL_USER,
+        password: MYSQL_PASSWORD,
+        database: MYSQL_DATABASE_NEWS,
+    });
+}
+
+// Lấy dữ liệu theo từng batch
+async function fetchBatch(sqlConnection, tableName, offset, limit) {
+    const query = `SELECT * FROM ${tableName} WHERE IdSite=${parseInt(OLD_SITE_ID)} LIMIT ${parseInt(limit)} OFFSET ${parseInt(offset)}`;
+    const [rows] = await sqlConnection.execute(query);
+
+    console.log(`🟢 Lấy ${rows.length} bản ghi từ ${tableName} (Offset: ${offset})`);
+    return rows;
+}
+
+async function migrateTable(sqlConnection, mongoDb, tableName) {
+    console.log(`🔄 Đang di chuyển bảng ${tableName}...`);
+
+    let offset = 0;
+    while (true) {
+        const rows = await fetchBatch(sqlConnection, tableName, offset, BATCH_SIZE);
+
+
+        const mappedNews = rows.map((row) => ({
+            slug: row.Slug,
+            url_img: row.ThumbnailFileUrl ? row.ThumbnailFileUrl.replace("https://cdn4t.mobiedu.vn", "https://media-moocs.mobifone.vn") : "",
+            title: row.Title,
+            short_description: row.Description,
+            description: row.HtmlContent.replaceAll("https://cdn4t.mobiedu.vn", "https://media-moocs.mobifone.vn"),
+            status: row.ApproveStatus,
+            createdAt: row.CreatedAt,
+            category: null,
+            siteId: +NEW_SITE_ID,
+            view_count: row.ViewCounter,
+            view_fake: 0,
+            pin_type: 0,
+        }));
+
+        // Kiểm tra nếu rows trống thì thoát khỏi vòng lặp
+        if (!rows || rows.length === 0) break;
+
+        await mongoDb.collection('news').insertMany(mappedNews);
+
+        offset += BATCH_SIZE;
+    }
+
+    console.log(`🏁 Hoàn tất di chuyển bảng ${tableName}!`);
+}
+
+const sqlConnection = await connectMySQL();
+
+// Chạy quá trình di chuyển
+async function migrate() {
+    const mongoClient = new MongoClient(MONGO_URL);
+    await mongoClient.connect();
+    const mongoDb = mongoClient.db(MONGO_DATABASE);
+
+    try {
+        await migrateTable(sqlConnection, mongoDb, 'Post');
+    } catch (error) {
+        console.error("❌ Lỗi khi di chuyển dữ liệu:", error);
+    } finally {
+        await sqlConnection.end();
+        await mongoClient.close();
+        console.log("🔚 Đã hoàn thành quá trình di chuyển.");
+    }
+}
+
+migrate();
--- user.js
+++ user.js
@@ -1,30 +1,130 @@
+import mysql from "mysql2/promise";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-const user = {
-    accessFailedCount :0,
-    address: "",
-    avatar:"",
-    birthday,
-    dateLogin,
-    deletedOn: null,
-    email:"adminsite@gmail.com",
-    fullname: "Admin Site",
-    gender: 1,
-    exploreField: 1  ,
-    siteId: 1,
-    positionId:1,
-    infoManagementLevel:null,
-    isLockoutEnabled : true,
-    lockoutEndDate : null,
-    passwordHash : "AQAAAAIAAYagAAAAEFwkvUkOhM2GpDB4DXa1MmKyLRpukZrolfwWQgYIsYJd//LFS0PlZI…",
-    phone :"0906258989",
-    pwd : "Unknown",
-    securityStamp : "DVBXFQKRZQPEOWO3UED6S62SRXQMIXVQ",
-    status : 1,
-    timeUpdate : null,
-    userName : "adminsite@gmail.com",
-    listRoles  : null,
-    listPolicy : null,
-    role : "ADMIN",
-    functionsTree : null,
+// Load biến môi trường từ .env
+dotenv.config();
 
+// Đọc cấu hình từ .env
+const {
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE_AUTH,
+    MONGO_URL,
+    MONGO_DATABASE,
+    BATCH_SIZE,
+    NEW_SITE_ID,
+    MYSQL_DATABASE_ROLE,
+    OLD_SITE_ID,
+} = process.env;
+
+
+// Kết nối MySQL
+async function connectMySQL() {
+    return mysql.createConnection({
+        host: MYSQL_HOST,
+        user: MYSQL_USER,
+        password: MYSQL_PASSWORD,
+        database: MYSQL_DATABASE_AUTH,
+    });
 }
+
+async function connectTableRole() {
+    return mysql.createConnection({
+        host: MYSQL_HOST,
+        user: MYSQL_USER,
+        password: MYSQL_PASSWORD,
+        database: MYSQL_DATABASE_ROLE,
+    });
+}
+
+// Lấy dữ liệu theo từng batch
+async function fetchBatch(sqlConnection, tableName, offset, limit) {
+    const query = `SELECT * FROM ${tableName} WHERE IdSite=${OLD_SITE_ID} LIMIT ${parseInt(limit)} OFFSET ${parseInt(offset)}`;
+    const [rows] = await sqlConnection.execute(query);
+
+    console.log(`🟢 Lấy ${rows.length} bản ghi từ ${tableName} (Offset: ${offset})`);
+    return rows;
+}
+
+async function findRole(records) {
+    if (records.length == 0) return [];
+    const ids = records.map(item => item.Id)
+    const query = `SELECT * FROM users WHERE mobiedu_user_id IN (${ids.join(",")})`;
+    const [rows] = await sqlConnectionRole.execute(query);
+    return rows;
+}
+
+async function migrateTable(sqlConnection, mongoDb, tableName) {
+    console.log(`🔄 Đang di chuyển bảng ${tableName}...`);
+
+    let offset = 0;
+    while (true) {
+        const rows = await fetchBatch(sqlConnection, tableName, offset, BATCH_SIZE);
+
+        const roles = await findRole(rows);
+
+        const mappedUsers = rows.map((row) => ({
+            accessFailedCount: 0,
+            address: "",
+            avatar: row.AvatarUrl ? row.AvatarUrl.replace("https://cdn4t.mobiedu.vn", "https://media-moocs.mobifone.vn") : "",
+            birthday: new Date(row.Birthday),
+            dateLogin: row.LastLoginDate,
+            deletedOn: null,
+            email: row.Email || "",
+            fullname: row.FullName,
+            gender: row.IdGender || 0,
+            exploreField: 1,
+            siteId: +NEW_SITE_ID,
+            positionId: "",
+            infoManagementLevel: null,
+            isLockoutEnabled: row.IsBlocked == 0 ? true : false,
+            lockoutEndDate: null,
+            passwordHash: row.Pwd,
+            phone: row.Phone || "",
+            pwd: "Migrate@2025",
+            securityStamp: "",
+            status: 1,
+            timeUpdate: null,
+            userName: row.Email,
+            listRoles: null,
+            listPolicy: null,
+            role: roles.find(item => item.mobiedu_user_id == row.Id)?.role == "student" ? "STUDENT" : "ADMIN",
+            functionsTree: null,
+        }));
+
+        // Kiểm tra nếu rows trống thì thoát khỏi vòng lặp
+        if (!rows || rows.length === 0) {
+            break;
+        }
+
+        await mongoDb.collection('user').insertMany(mappedUsers);
+
+        offset += BATCH_SIZE;
+    }
+
+    console.log(`🏁 Hoàn tất di chuyển bảng ${tableName}!`);
+}
+
+const sqlConnectionRole = await connectTableRole()
+const sqlConnection = await connectMySQL();
+
+// Chạy quá trình di chuyển
+async function migrate() {
+    const mongoClient = new MongoClient(MONGO_URL);
+    await mongoClient.connect();
+    const mongoDb = mongoClient.db(MONGO_DATABASE);
+
+    try {
+        await migrateTable(sqlConnection, mongoDb, 'Users');
+    } catch (error) {
+        console.error("❌ Lỗi khi di chuyển dữ liệu:", error);
+    } finally {
+        await sqlConnection.end();
+        await mongoClient.close();
+        console.log("🔚 Đã hoàn thành quá trình di chuyển.");
+    }
+}
+
+migrate();
